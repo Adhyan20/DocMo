e82b44631f93bfcfec636a6fe298d15c18794429
Adhyan20 <adhyank7@gmail.com>
2024-12-05 22:19:15 +0530

    DocMo

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = ({ children, className }: HeaderProps) => {
+  return (
+    <div className={cn("header", className)}>
+      <Link href='/' className="md:flex-1">
+
+
+      </Link>
+      {children}
+    </div>
+  )
+}
+
+export default Header
